16bdc38e38a660d843992e763e848f6300514969
Sudhamshi Sowmya <sudhamshisowmya@gmail.com>
2026-08-03 19:25:51 +0530

    docs: update project screenshots and fix CORS configuration

--- api-gateway/src/main/java/com/aquabill/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/aquabill/apigateway/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.aquabill.api_gateway.config;
+package com.aquabill.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
